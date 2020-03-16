e91230665ce338c65055beeb4f8b0e2f6500c1ab
Luiz Carlos <luiz@migrasoft.com.br>
2020-03-15 23:02:09 -0300

    Acrescentada a mensagem desejando um bom dia

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -12,6 +12,7 @@ namespace HelloWorld
             Console.WriteLine("Hello World");
             Console.WriteLine("Good Morning");
             Console.WriteLine("Como você está hoje?");
+            Console.WriteLine("Tenha um bom dia");
 
             Console.ReadLine();
         }
